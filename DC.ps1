ff4e24b939c86bdf735a9b32fdc42679132405f0
1mm0rt41PC <guillaumenuel@hotmail.com>
2022-08-27 22:56:30 +0200

    Update DC.ps1

--- DC.ps1
+++ DC.ps1
@@ -11,7 +11,7 @@ $ComputerName   = 'DC-SRV01'
 ###############################################################################
 # AD-SRV01
 Rename-Computer -NewName $ComputerName
-New-NetIPAddress –IPAddress $IP_AD -DefaultGateway $IP_GATEWAY -PrefixLength 24 -InterfaceIndex (Get-NetAdapter).InterfaceIndex
+New-NetIPAddress -IPAddress $IP_AD -DefaultGateway $IP_GATEWAY -PrefixLength 24 -InterfaceIndex (Get-NetAdapter).InterfaceIndex
 Set-DNSClientServerAddress –InterfaceIndex (Get-NetAdapter).InterfaceIndex –ServerAddresses $IP_DNS
 Install-WindowsFeature -name AD-Domain-Services -IncludeManagementTools
 Install-ADDSForest -DomainName $domain -DomainNetBIOSName $domain_netbios -InstallDNS:$true -DomainMode WinThreshold -ForestMode WinThreshold -Force:$true
