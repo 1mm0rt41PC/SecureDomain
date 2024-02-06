1f5ec4db2bcd7129c052f013e5237b569e95adcb
1mm0rt41PC <5358076+1mm0rt41PC@users.noreply.github.com>
2024-02-06 14:52:30 +0100

    Update Coercing-Blocker.ps1

--- Coercing-Blocker.ps1
+++ Coercing-Blocker.ps1
@@ -39,6 +39,14 @@ New-GPO -Name "[Firewall](GPO,Computer) Default rules for DC" | %{
 	New-NetFirewallRule -Enabled True -Profile Any -ErrorAction Continue -GPOSession $GpoSessionName -DisplayName "[GPO] Allow * UDP except 3389" -Action Allow -Direction Outbound -RemotePort @('0-444','446-65535') -Protocol UDP >$null
 	New-NetFirewallRule -Enabled True -Profile Any -ErrorAction Continue -GPOSession $GpoSessionName -DisplayName "[GPO] Allow * for DC" -Action Allow -Direction Outbound -RemoteAddress @('192.168.1.1','192.168.1.2') >$null
 	
+	New-NetFirewallRule -Enabled True -Profile Any -ErrorAction Continue -GPOSession $GpoSessionName -DisplayName "[GPO] Allow * for AATPSensor" -Action Allow -Direction Outbound -Service AATPSensor >$null
+	
+	New-NetFirewallRule -Enabled True -Profile Any -ErrorAction Continue -GPOSession $GpoSessionName -DisplayName "[GPO] Allow * TCP except 3389" -Action Allow -Direction Outbound -RemotePort @('0-444','446-65535') -Protocol TCP >$null
+	New-NetFirewallRule -Enabled True -Profile Any -ErrorAction Continue -GPOSession $GpoSessionName -DisplayName "[GPO] Allow * UDP except 3389" -Action Allow -Direction Outbound -RemotePort @('0-444','446-65535') -Protocol UDP >$null
+	
+	New-NetFirewallRule -Enabled True -Profile Any -ErrorAction Continue -GPOSession $GpoSessionName -DisplayName "[GPO] Allow * ICMP" -Action Allow -Direction Outbound -Protocol ICMPv4 >$null
+	New-NetFirewallRule -Enabled True -Profile Any -ErrorAction Continue -GPOSession $GpoSessionName -DisplayName "[GPO] Allow * ICMP" -Action Allow -Direction Inbound -Protocol ICMPv4 >$null
+	
 	Set-NetFirewallProfile -ErrorAction Continue -GPOSession $GpoSessionName -All -AllowLocalFirewallRules False -LogAllowed False -LogBlocked True -LogIgnored True -LogFileName "%windir%\system32\logfiles\pfirewall.log" -LogMaxSizeKilobytes 32767
 	Save-NetGPO -GPOSession $GpoSessionName >$null
 	
