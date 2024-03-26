93679b59bdb65969a39a28eef65ba2c8086cc5e5
1mm0rt41PC <5358076+1mm0rt41PC@users.noreply.github.com>
2024-03-26 09:46:50 +0100

    Update LDAP-Signing.ps1

--- LDAP-Signing.ps1
+++ LDAP-Signing.ps1
@@ -1,15 +1,25 @@
+#
+# Enable LDAP Sigging on client side
+#
 New-GPO -Name "[1mm0rt41][Security](GPO,Computer) LDAP client configuration" | %{
 	$_ | Set-GPRegistryValue -Key "HKLM\System\CurrentControlSet\Services\LDAP" -ValueName "LDAPClientIntegrity" -Value 2 -Type DWord >$null
 	$_
 } | New-GPLink -target "$(([ADSI]'LDAP://RootDSE').defaultNamingContext.Value)" -LinkEnabled Yes
 
 
+#
+# Enable LDAP Sigging on SERVER side
+#
 New-GPO -Name "[1mm0rt41][Security](GPO,Computer) LDAP server configuration" | %{
 	$_ | Set-GPRegistryValue -Key "HKLM\System\CurrentControlSet\Services\NTDS\Parameters" -ValueName "LDAPServerIntegrity" -Value 2 -Type DWord >$null
 	$_ | Set-GPRegistryValue -Key "HKLM\System\CurrentControlSet\Services\NTDS\Parameters" -ValueName "LdapEnforceChannelBinding" -Value 2 -Type DWord >$null
 	$_
-} | New-GPLink -target "OU=Domain Controllers,$(([ADSI]'LDAP://RootDSE').defaultNamingContext.Value)" -LinkEnabled Yes
+}
 
+
+#
+# Enable AUDIT LDAP Sigging (no LDAPS CB) on SERVER side
+#
 New-GPO -Name "[1mm0rt41][Audit](GPO,Computer) Audit LDAP SASL" -Comment "Log missing LDAP SASL => event ID of 2889 in the Directory Service log. Monitoring for LDAP Binding without Channel Binding
  => event ID 3039 in the Directory Service event log" | %{
   $_ | Set-GPRegistryValue -Key "HKLM\SYSTEM\CurrentControlSet\Services\NTDS\Diagnostics" -ValueName "16 LDAP Interface Events" -Value 2 -Type DWORD >$null
@@ -17,4 +27,4 @@ New-GPO -Name "[1mm0rt41][Audit](GPO,Computer) Audit LDAP SASL" -Comment "Log mi
   $_ | Set-GPRegistryValue -Key "HKLM\SYSTEM\CurrentControlSet\Services\EventLog\Directory Service" -ValueName "MaxSize" -Value 33685504 -Type DWORD >$null
   $_ | Set-GPRegistryValue -Key "HKLM\SYSTEM\CurrentControlSet\Services\EventLog\Directory Service" -ValueName "MaxSizeUpper" -Value 0 -Type DWORD >$null
   $_
-} | New-GPLink -target "OU=Domain Controllers,$(([ADSI]'LDAP://RootDSE').defaultNamingContext.Value)" -LinkEnabled Yes
+}
