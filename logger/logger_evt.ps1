0db34c6b6ff682e21e10faf9e7ab210b35634b8e
1mm0rt41PC <5358076+1mm0rt41PC@users.noreply.github.com>
2024-06-21 14:52:09 +0200

    Update logger_evt.ps1

--- logger/logger_evt.ps1
+++ logger/logger_evt.ps1
@@ -1,4 +1,26 @@
 <#
+# logger_evt.ps1 - A simple script that automates Windows collect events info
+#
+# Filename: logger_evt.ps1
+# Author: 1mm0rt41PC - immortal-pc.info - https://github.com/1mm0rt41PC
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; see the file COPYING. If not, write to the
+# Free Software Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+#
+# Update: 2024-06-21 - Add auto cleanup
+#>
+<#
 # LOG Server
 # ================================================
 $logs = "C:\Windows\SYSVOL\domain\logs"
