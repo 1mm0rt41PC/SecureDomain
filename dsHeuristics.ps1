cdd7c18978511c56cc14a7d4463ffc7dc8b9eb11
1mm0rt41PC <5358076+1mm0rt41PC@users.noreply.github.com>
2024-09-06 23:51:58 +0200

    Update dsHeuristics.ps1

--- dsHeuristics.ps1
+++ dsHeuristics.ps1
@@ -7,3 +7,21 @@ if(($dsHeuristics -eq "") -or ($dsHeuristics.Length -lt 7)){
 Write-Host "$msg"
 $msg | Out-File -Encoding ASCII dsHeuristics.log
 Write-Host "Do it on all domain"
+
+<#
+fLDAPBlockAnonOps permet d'autoriser des opérations LDAP sans authentification ;
+fAllowAnonNSPI permet d'autoriser l'accès anonyme au Name Service Provider Interface (NSPI) ;
+dwAdminSDExMask permet de définir les groupes protégés par le mécanisme SDProp ;
+DoNotVerifyUPNAndOrSPNUniqueness permet de relaxer les vérifications sur l'unicité des UPN et SPN ;
+LDAPAddAuthZVerifications permet de désactiver l'audit et la protection apportée par la KB5008383 ;
+BlockOwnerImplicitRights permet de désactiver l'audit et la protection apportée par laKB5008383.
+
+Les paramètres dangereux configurés dans la propriété dSHeuristics doivent être modifiés et réinitialisés à leur valeur par défaut :
+
+fLDAPBlockAnonOps ne doit pas être configuré ou avoir une valeur différente de 2 ;
+fAllowAnonNSPI doit valoir 0 ;
+dwAdminSDExMask doit valoir 0 ;
+DoNotVerifyUPNAndOrSPNUniqueness doit valoir 0 ;
+LDAPAddAuthZVerifications ne doit pas être configuré ou avoir une valeur différente de 2 pour atteindre le niveau 3 ; cette valeur doit être explicitement positionnée à 1 pour atteindre le niveau 5 ;
+BlockOwnerImplicitRights ne doit pas être configuré ou avoir une valeur différente de 2 pour atteindre le niveau 3 ; cette valeur doit être explicitement positionnée à 1 pour atteindre le niveau 5.
+#>
